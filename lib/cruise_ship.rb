ca3038ef91ef40edbeb4fbc40b5d3f68c84cd3a0
Delia Constantino <dconstan92@gmail.com>
2020-10-31 19:29:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,34 +6,12 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-
-
 def select_winner(passengers)
-  #iterate over each hash pair, need to select a person in key suite_a that starts with value "A"
-  passengers.each do |suite, passenger|
-    if suite == :suite_a
-      return passenger if passenger.start_with?("A")
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
     end
   end
+  winner
 end
-
-
-
-
-
-
-
-
-
-
-
-
-# def select_winner(passengers)
-#   winner = ""
-#   passengers.each do |suite, name|
-#     if suite == :suite_a && name.start_with?("A")
-#       winner = name
-#     end
-#   end
-#   winner
-# end
