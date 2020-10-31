ca502717f0656f93b4d0bc261451573d50aa8515
Delia Constantino <dconstan92@gmail.com>
2020-10-31 19:35:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,50 +4,19 @@
 #   "Amanda" => 27
 # }
 
-
 def happy_birthday(birthday_kids)
-  #need to iterate over our has and put out ind message to each kid
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
 
-def age_appropriate_birthday(bday_hash)
-  bday_hash.each do |name, age|
-    if age <= 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+      if age <= 12
+        puts "Happy Birthday #{name}! You are now #{age} years old!"
+      else
+        puts "You are too old for this."
+      end
     end
-  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def happy_birthday(birthday_kids)
-#   birthday_kids.each do |name, age|
-#     puts "Happy Birthday #{name}! You are now #{age} years old!"
-#   end
-# end
-#
-#
-# def age_appropriate_birthday(birthday_kids)
-#   birthday_kids.each do |name, age|
-#       if age <= 12
-#         puts "Happy Birthday #{name}! You are now #{age} years old!"
-#       else
-#         puts "You are too old for this."
-#       end
-#     end
-# end
